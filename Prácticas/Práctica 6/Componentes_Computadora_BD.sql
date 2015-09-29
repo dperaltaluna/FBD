bc20982f58c3dd9ef045e4769a4383be86bb52ae
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-09-28 22:32:42 -0500

    Se agregan las llaves primarias y foraneas.

---
+++ Prácticas/Práctica 6/Componentes_Computadora_BD.sql
@@ -0,0 +1,12 @@
+﻿-- Database: "Componentes_Computadora_BD"
+
+-- DROP DATABASE "Componentes_Computadora_BD";
+
+CREATE DATABASE "Componentes_Computadora_BD"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Spain.1252'
+       LC_CTYPE = 'Spanish_Spain.1252'
+       CONNECTION LIMIT = -1;
+
