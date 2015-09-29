1f15c8d109ef41577a6bdc5f7022c4f4c74da315
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-09-28 22:27:20 -0500

    Se agregan correcciones de la definicion de la BD.

--- Prácticas/Práctica 5/Componentes_Computadora_BD.sql
+++ Prácticas/Práctica 5/Componentes_Computadora_BD.sql
@@ -1,14 +1,24 @@
--- Table: "Interfaz_Tarjeta_de_Red"
+-- Table: "Cliente"
 
--- DROP TABLE "Interfaz_Tarjeta_de_Red";
+-- DROP TABLE "Cliente";
 
-CREATE TABLE "Interfaz_Tarjeta_de_Red"
+CREATE TABLE "Cliente"
 (
-  id_tarjeta_de_red integer,
-  interfaz_tarjeta_de_red character varying(20)
+  rfc_cliente character varying(13) NOT NULL,
+  nombre_cliente character varying(20),
+  apellido_paterno_cliente character varying(20),
+  apellido_materno_cliente character varying(20),
+  fecha_nacimiento_cliente date,
+  nombre_calle_cliente character varying(20),
+  colonia_cliente character varying(20),
+  delegacion_cliente character varying(20),
+  estado_cliente character varying(20),
+  ciudad_cliente character varying(20),
+  numero_interior_cliente integer,
+  CONSTRAINT pk_rfc_cliente PRIMARY KEY (rfc_cliente)
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE "Interfaz_Tarjeta_de_Red"
+ALTER TABLE "Cliente"
   OWNER TO postgres;
