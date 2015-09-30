394146778a76b210acf29d24f981d0ce9f61f642
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-09-29 21:04:52 -0500

    Se agrega la version enviada de la practica 6 sobre Integridad.

--- Prácticas/Práctica 6/Componentes_Computadora_BD.sql
+++ Prácticas/Práctica 6/Componentes_Computadora_BD.sql
@@ -1,12 +1,16 @@
-﻿-- Database: "Componentes_Computadora_BD"
+﻿-- Table: "Tipo_Audio_Tarjeta_Sonido"
 
--- DROP DATABASE "Componentes_Computadora_BD";
+-- DROP TABLE "Tipo_Audio_Tarjeta_Sonido";
 
-CREATE DATABASE "Componentes_Computadora_BD"
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'Spanish_Spain.1252'
-       LC_CTYPE = 'Spanish_Spain.1252'
-       CONNECTION LIMIT = -1;
-
+CREATE TABLE "Tipo_Audio_Tarjeta_Sonido"
+(
+  id_tarjeta_de_sonido integer NOT NULL,
+  tipo_audio character varying(8) NOT NULL,
+  CONSTRAINT pk_tipo_tarjeta_sonido PRIMARY KEY (id_tarjeta_de_sonido, tipo_audio),
+  CONSTRAINT chk_id_tarjeta_de_sonido CHECK (id_tarjeta_de_sonido > 0 AND id_tarjeta_de_sonido <= 999999999)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Tipo_Audio_Tarjeta_Sonido"
+  OWNER TO postgres;
