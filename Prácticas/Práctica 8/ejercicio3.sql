f3142038b3ddb693210566f7409b8d3642d74e97
gabsnake20 <gabsnake20@gmail.com>
2015-10-15 18:39:03 -0500

    Ejercicio 3 agregada consulta

--- Prácticas/Práctica 8/ejercicio3.sql
+++ Prácticas/Práctica 8/ejercicio3.sql
@@ -1,3 +1,11 @@
+--ejercicio 3
+--consulta que devuelve los datos de los clientes que hanrealizado un pedido
+SELECT * 
+FROM "Cliente" LEFT JOIN "Pedido" 
+ON ("Cliente".rfc_cliente = "Pedido".rfc_cliente);
+
+--consulta que regresa los clientes de los cuales contamos con us
+--datosde contacto
 SELECT rfc_cliente AS "RFC",(nombre_cliente,apellido_paterno_cliente,apellido_materno_cliente) AS "NOMBRE",
  correo_cliente AS "Email", telefono_cliente As "TELEFONO"
 FROM "Cliente" NATURAL JOIN "Correo_Cliente" NATURAL JOIN "Telefono_Cliente";
