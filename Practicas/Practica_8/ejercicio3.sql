3c3ad47ca506a9aa6e70b66034c388fbb9cca676
gabsnake20 <gabsnake20@gmail.com>
2015-10-15 18:58:30 -0500

    agregados los comentarios

--- Practicas/Practica_8/ejercicio3.sql
+++ Practicas/Practica_8/ejercicio3.sql
@@ -4,8 +4,7 @@ SELECT *
 FROM "Cliente" LEFT JOIN "Pedido" 
 ON ("Cliente".rfc_cliente = "Pedido".rfc_cliente);
 
---consulta que regresa los clientes de los cuales contamos con us
---datosde contacto
+--consulta para obtenerf los clientes que ya han proporcionado su telefono y mail
 SELECT rfc_cliente AS "RFC",(nombre_cliente,apellido_paterno_cliente,apellido_materno_cliente) AS "NOMBRE",
  correo_cliente AS "Email", telefono_cliente As "TELEFONO"
 FROM "Cliente" NATURAL JOIN "Correo_Cliente" NATURAL JOIN "Telefono_Cliente";
