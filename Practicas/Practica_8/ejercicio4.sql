9af40754cb746db0947875986e2c9b731bee0ba5
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-10-15 23:46:31 -0500

    Se agrega el script del ejercicio 4.

---
+++ Practicas/Practica_8/ejercicio4.sql
@@ -0,0 +1,10 @@
+﻿-- Ejercicio 4
+-- Consulta que devuelve los clientes que ya hicieron pedidos 
+-- y que viven en el Distrito Federal, con maximo numero de pedido 
+-- menor al numero 209, ordenado por ciudad y por nombre del cliente.
+
+SELECT ciudad_cliente, nombre_cliente, max(numero_pedido) AS maximo_numero_pedido
+    FROM "Cliente" INNER JOIN "Pedido" ON ("Cliente".rfc_cliente = "Pedido".rfc_cliente)
+    WHERE "Cliente".ciudad_cliente = 'Distrito Federal'
+    GROUP BY "Cliente".ciudad_cliente, "Cliente".nombre_cliente
+    HAVING max(numero_pedido) < 209;
