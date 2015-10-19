6094c707d22396683fb2843012d1a134df71000e
Alma Vega <almaw@ciencias.unam.mx>
2015-10-18 19:51:07 -0500

    Ejercicio 2 terminado

--- Practicas/Practica_8/ejercicio2.sql
+++ Practicas/Practica_8/ejercicio2.sql
@@ -6,4 +6,15 @@ FROM "Procesador"
 WHERE nucleos_fisicos_procesador >= 4 
 GROUP BY modelo_procesador;
 
+--Consulta 2: Pagos por cliente 
+select rfc_cliente,sum(cantidad_pago)
+from "Pago"
+group by rfc_cliente
+
+--Consulta 3: Cantidad de productos por cliente
+select rfc_cliente,sum(cantidad_pedido) 
+from "Pedido"
+group by rfc_cliente 
+
+
 
