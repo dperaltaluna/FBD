83efad639cc834169ad7473f840d31e5b22e27f7
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-10-30 13:48:57 -0600

    Se agregan los ejercicios 4 y 8.

---
+++ Tareas/Tarea_3/tarea3_diego.sql
@@ -0,0 +1,21 @@
+﻿-- Ejercicio 4
+-- Nombre de todos los equipos y los estadios.
+
+SELECT nombre_equipo AS nombre_equipos_estadios
+FROM equipo
+UNION
+SELECT nombre_estadio
+FROM estadio;
+
+
+
+-- Ejercicio 8
+-- Los nombres de los jugadores y su fecha de adquisición
+-- tales que sean Receiver o Punter, pero su fecha
+-- de adquisición sea mayor a 2002. 
+-- Utiliza para ello el operador de UNION
+
+SELECT nombre_jugador, anyo_adquirido
+FROM jugador INNER JOIN jugador_equipo ON jugador.id_jugador = jugador_equipo.id_jugador
+INNER JOIN receiver ON jugador_equipo.id_jugador = receiver.id_jugador
+WHERE anyo_adquirido > 2002;
