f0889e0526ad0a2fbc21c17d2057c478c84b69cc
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-11-03 21:01:49 -0600

    Se agregan lo ejercicios 20 y 24.

--- Tareas/Tarea_3/tarea3_diego.sql
+++ Tareas/Tarea_3/tarea3_diego.sql
@@ -1,5 +1,6 @@
 ﻿-- Ejercicio 4
--- Devuelve una tabla con el nombre de todos los equipos y los estadios.
+-- Devuelve una tabla con el nombre de todos los equipos y 
+-- los estadios.
 
 SELECT nombre_equipo AS nombre_equipos_estadios
 FROM equipo
@@ -28,8 +29,9 @@ WHERE anyo_adquirido > 2002;
 
 
 -- Ejercicio 12
--- Devuelve una tabla con el numero de jugadores con apellidos paternos distintos.
--- 
+-- Devuelve una tabla con el numero de jugadores con 
+-- apellidos paternos distintos.
+
 SELECT COUNT(DISTINCT(apellido_jugador))
 FROM jugador;
 
@@ -45,3 +47,41 @@ FROM
 	 GROUP BY nombre_ciudad)
 	 AS ciudades_mayor_num_equipos(nombre_ciudad, mayor_num_equipos)
 GROUP BY nombre_ciudad;
+
+
+
+-- Ejercicio 20
+-- Datos de los partidos de los equipos con el mayor 
+-- record de juegos perdidos. 
+-- Se elije un umbral de 8 partidos record perdidos 
+-- en adelante. El maximo es 15.
+
+SELECT *
+FROM
+(SELECT equipo.id_equipo, MAX(record_p)
+		    FROM equipo
+		    GROUP BY equipo.id_equipo
+		    HAVING record_p > 8) AS equipo_record_perdidos(id_equipo, record_perdidos)
+INNER JOIN partido ON (id_equipo = id_equipo_l)
+ORDER BY record_perdidos DESC;
+
+
+
+-- Ejercicio 24
+-- 
+-- Todos los datos de los entrenadores que hayan participado 
+-- en los partidos de la semana 5 y nombre de los equipos 
+-- que entrenaron.
+
+SELECT entrenador.id_entrenador, apellido_entrenador, nombre_entrenador, cargo, nombre_equipo
+FROM
+(SELECT *
+FROM
+(SELECT *
+FROM
+(SELECT *
+FROM partido
+WHERE semana = 5) as partidos_semana_cinco
+INNER JOIN equipo_entrenador ON (id_equipo = id_equipo_l OR id_equipo = id_equipo_v)) AS entrenadores_equipos_semana5
+INNER JOIN equipo ON (entrenadores_equipos_semana5.id_equipo = equipo.id_equipo)) AS entrenadores_equipos_semana5_nombre_entrenador
+INNER JOIN entrenador ON (entrenador.id_entrenador = entrenadores_equipos_semana5_nombre_entrenador.id_entrenador)
