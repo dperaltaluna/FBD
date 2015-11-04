2ee0d5bbcb2ff08d26c89445f0c0d626beefe657
nekixito <nekixito@gmail.com>
2015-11-03 20:03:04 -0600

    Create tarea3_miguel.sql
    Ejercicios tarea

---
+++ Tareas/Tarea_3/tarea3_miguel.sql
@@ -0,0 +1,30 @@
+--Ejercicio 2 Estadios tales que su anio de construccion es mayor a 1978
+SELECT *
+FROM "estadio"
+WHERE anyo_construccion>1978;
+
+--Ejercicio 6 jugadores y equipos tales que su anio de adquisicion sea 2006 o 2003
+SELECT id_jugador, nombre_jugador, apellido_jugador, id_equipo, anyo_adquirido 
+FROM "jugador_equipo"
+NATURAL JOIN "jugador"
+WHERE anyo_adquirido = 2003 or anyo_adquirido = 2006;
+
+--Ejercicio 10 Datos de los equipos con mayor numero de juegos ganados
+SELECT *
+FROM "equipo"
+WHERE record_g = (SELECT MAX(record_g) FROM "equipo");
+
+--Ejercicio 14 Cuantos equipos hay pertenecientes a cada división
+
+SELECT division, count(*) AS "Pertenecen"
+FROM equipo
+WHERE division = 'Oeste' or division = 'Sur' or division = 'Este' or division = 'Norte' 
+GROUP BY division;
+
+
+--Ejercicio 18 De las ciudades con equipos asociados. Equipo mayor juegos ganados.
+
+SELECT nombre_ciudad, record_g
+FROM (SELECT * FROM "ciudad" NATURAL JOIN "equipo_ciudad") AS "Ciudad_mayor_ganados"
+NATURAL JOIN "equipo"
+WHERE record_g = (SELECT MAX(record_g) FROM "equipo");
