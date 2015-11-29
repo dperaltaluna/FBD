8e9fd01963d8f08d1009d3797348577418557437
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-11-29 17:08:49 -0600

    Se agregan inserts que violan algun check.

---
+++ proyecto_final/insert_into_integridad/insert_into_integridad.sql
@@ -0,0 +1,11 @@
+﻿-- Inserts que violan restricciones de integridad.
+
+INSERT INTO cliente (id_cliente,nombre,apellido_paterno,apellido_materno,usuario,pasword,telefono,email,numero_tarjetac,numero,calle,colonia,ciudad,pais) VALUES (-10021,'Daniela','Little','Horton','Dany','2125','02 32 23 16 55','dany@outlook.com','22527016755895','93','San Pablo','Santa Ursula','Distrito Federal','Mexico');
+
+-- INSERT INTO producto (id_producto,nombre,categoria,precio) VALUES (2485,'procesador','DB-computo',0);
+-- 
+-- INSERT INTO "proveedor" (id_proveedor,nombre,direccion,email) VALUES (-1991,'Gravida','7927 Laoreet Ave','support@gravidaPraesenteu.edu');
+
+
+
+
