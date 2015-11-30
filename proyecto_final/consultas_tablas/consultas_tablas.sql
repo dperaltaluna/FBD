b6b23555ce60ed0b332859520f4022f00b176d10
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-11-29 19:18:20 -0600

    Se agrega la primera consulta.

---
+++ proyecto_final/consultas_tablas/consultas_tablas.sql
@@ -0,0 +1,10 @@
+﻿--SELECT EXTRACT (YEAR FROM(fecha))
+
+-- 1. Clientes y pedidos que hayan comprado el mismo producto 
+-- en el año 2015 y que su país sea Japan.
+
+SELECT *
+FROM (cliente AS c INNER JOIN (cliente_pedido AS cp
+	INNER JOIN pedido_producto AS pp ON (cp.id_pedido = pp.id_pedido)) as r 
+	ON (c.id_cliente = r.id_cliente))
+WHERE EXTRACT(YEAR FROM(fecha)) = 2015 AND c.pais = 'Japan';
