7b1e4047c0dd655eb22b20596549216102619905
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-11-29 20:21:24 -0600

    Se agrega consulta 3 y se actualizan las 1 y 2.

--- proyecto_final/consultas_tablas/consultas_tablas.sql
+++ proyecto_final/consultas_tablas/consultas_tablas.sql
@@ -1,17 +1,36 @@
-﻿--SELECT EXTRACT (YEAR FROM(fecha))
+﻿-- Script de consultas.
 
 -- 1. Clientes y pedidos que hayan comprado el mismo producto 
 -- en el año 2015 y que su país sea Japan.
 
 SELECT *
-FROM (cliente AS c INNER JOIN (cliente_pedido AS cp
-	INNER JOIN pedido_producto AS pp ON (cp.id_pedido = pp.id_pedido)) as r 
+FROM (cliente c INNER JOIN (cliente_pedido cp
+	INNER JOIN pedido_producto pp ON (cp.id_pedido = pp.id_pedido)) as r 
 	ON (c.id_cliente = r.id_cliente))
 WHERE EXTRACT(YEAR FROM(fecha)) = 2015 AND c.pais = 'Japan';
+
 
 -- 2. Los clientes que no hayan echo ningún pedido durante el 
 -- mes de octubre de cualquier año.
 
-SELECT *
-FROM (cliente_pedido AS cp INNER JOIN cliente AS c ON (cp.id_cliente = c.id_cliente))
+SELECT cli.id_cliente, cp.id_pedido, cp.fecha, cli.nombre, cli.apellido_paterno, cli.apellido_materno
+FROM (cliente_pedido cp INNER JOIN cliente cli ON (cp.id_cliente = cli.id_cliente))
 WHERE EXTRACT(MONTH FROM(fecha)) != 10;
+
+
+-- 3. Aquellos clientes y pedidos que empiecen con V y hayan 
+-- comprado al menos un producto.
+
+SELECT cl.id_cliente, cl.nombre, cl.apellido_paterno, cl.apellido_materno, cp.id_pedido, p.monto_final
+FROM cliente cl INNER JOIN cliente_pedido cp 
+     ON (cl.id_cliente = cp.id_cliente) 
+	INNER JOIN pedido p ON (cp.id_pedido = p.id_pedido)
+WHERE cl.nombre ~ '^V' AND p.monto_final > 0;
+
+
+-- 4. Para aquellos clientes que hayan comprado al menos un producto en el año 2014 
+-- con precio mayor a 50, mostrar una leyenda de 'Autorizado', si su costo es diferente 
+-- a 20 el cliente sera 'Verificado', aquellos clientes que se llamen Erin su leyenda 
+-- sera 'Activo', si su apellido es Perry el cliente sera 'Aprobando' 
+-- y en cualquier otro caso 'En proceso'.
+
