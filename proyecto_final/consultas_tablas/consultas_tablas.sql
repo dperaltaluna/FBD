83fcd03cc005cd975bfae7da0e957afcd78a2185
Diego Alejandro Peralta Luna <dperaltaluna@ciencias.unam.mx>
2015-11-29 19:28:48 -0600

    Se agrega propuesta de solucion consulta 2.

--- proyecto_final/consultas_tablas/consultas_tablas.sql
+++ proyecto_final/consultas_tablas/consultas_tablas.sql
@@ -8,3 +8,10 @@ FROM (cliente AS c INNER JOIN (cliente_pedido AS cp
 	INNER JOIN pedido_producto AS pp ON (cp.id_pedido = pp.id_pedido)) as r 
 	ON (c.id_cliente = r.id_cliente))
 WHERE EXTRACT(YEAR FROM(fecha)) = 2015 AND c.pais = 'Japan';
+
+-- 2. Los clientes que no hayan echo ningún pedido durante el 
+-- mes de octubre de cualquier año.
+
+SELECT *
+FROM (cliente_pedido AS cp INNER JOIN cliente AS c ON (cp.id_cliente = c.id_cliente))
+WHERE EXTRACT(MONTH FROM(fecha)) != 10;
